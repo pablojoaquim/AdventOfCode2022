81a7f5dcac348b1da8b76d4b622c36b8c9d31b04
Pablo Joaquim <pablojoaquim@gmail.com>
2022-12-24 18:38:57 -0300

    The second part of the challenge of the Day06 has been completed.

---
+++ day06.02/src/main.py
@@ -0,0 +1,72 @@
+# ******************************************************************************
+# * @file main.py
+# * @author Pablo Joaquim
+# * @brief The entry point
+# *
+# * @copyright NA
+# *
+# ******************************************************************************
+
+# ******************************************************************************
+# * import modules
+# ******************************************************************************
+import logging
+import signal
+
+# ******************************************************************************
+# * Objects Declarations
+# ******************************************************************************
+
+# ******************************************************************************
+# * Object and variables Definitions
+# ******************************************************************************
+running = True
+
+# ******************************************************************************
+# * Function Definitions
+# ******************************************************************************
+
+# ******************************************************************************
+# * @brief The handler for the termination signal handler
+# ******************************************************************************
+def sigintHandler(signum, frame):
+    global running
+    running = False
+    print('Signal handler called with signal', signum)
+    raise RuntimeError("Terminating...")
+
+
+# ******************************************************************************
+# * @brief The main entry point
+# ******************************************************************************
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, sigintHandler)
+
+    # These parameters are for the werkzeug embedded web server of Flask
+    # If we're using gunicorn (WSGI production web server) these parameters are not applied
+    try:
+        position = list()
+        print("Initializing...", flush=True)
+
+        # Open the file with the inputs
+        with open('tst/input.txt') as f:
+            # Move along the lines of the input file
+            for line in f:
+                # The separator is an EOL character
+                if(line != "\n"):
+                    # Remove the EOL character of every line
+                    line = line.replace('\n', '')
+                    
+                    # Look for 4 characters completely different of each others
+                    for i in range(len(line)):
+                        substring = line[i:i+14]
+                        print("Substring:" + str(substring), flush=True)
+                        if (len(set(substring)) == len(substring)):
+                            position.append(i+14)
+                            print("Position:" + str(position), flush=True)
+                            break               
+            
+        print("Position:" + str(position), flush=True)
+        
+    except RuntimeError:
+        print("Finishing...", flush=True)
