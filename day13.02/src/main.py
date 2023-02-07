afa4281d36fa2ba46550ee29c7430124cac5f1c5
Pablo Joaquim <pablojoaquim@gmail.com>
2023-02-07 01:41:52 -0300

    day13.02 - The distress signal has been calculated

--- day13.02/src/main.py
+++ day13.02/src/main.py
@@ -32,33 +32,35 @@ running = True
 def comparePackets(input1, input2):
     order = "equal"
     
+    cmp1 = input1.copy()
+    cmp2 = input2.copy()
     # print("- Compare ", input1, " vs ", input2)
         
-    for i in range(len(input1)):
-        if i<len(input2):
+    for i in range(len(cmp1)):
+        if i<len(cmp2):
             #If one of the elements is a list and the other is an integer
-            if(isinstance(input1[i], int) and isinstance(input2[i],list)):
-                aux = [input1[i]]
-                input1[i] = aux
-            elif(isinstance(input1[i], list) and isinstance(input2[i],int)):
-                aux = [input2[i]]
-                input2[i] = aux
+            if(isinstance(cmp1[i], int) and isinstance(cmp2[i],list)):
+                aux = [cmp1[i]]
+                cmp1[i] = aux
+            elif(isinstance(cmp1[i], list) and isinstance(cmp2[i],int)):
+                aux = [cmp2[i]]
+                cmp2[i] = aux
             
             # If both elements are integers
-            if(isinstance(input1[i], int) and isinstance(input2[i],int)):
+            if(isinstance(cmp1[i], int) and isinstance(cmp2[i],int)):
                 # print("  - Compare ", input1[i], " vs ", input2[i] )
-                if (input1[i]<input2[i]):
+                if (cmp1[i]<cmp2[i]):
                     order = "ok"
                     # print("  - Left side is smaller, so inputs are in the right order")
                     break
-                elif (input1[i]>input2[i]):
+                elif (cmp1[i]>cmp2[i]):
                     order = "nok"
                     # print(" - Right side is smaller, so inputs are not in the right order")                    
                     break
                 
             # If both elements are lists
-            elif(isinstance(input1[i], list) and isinstance(input2[i],list)):
-                order = comparePackets(input1[i], input2[i])
+            elif(isinstance(cmp1[i], list) and isinstance(cmp2[i],list)):
+                order = comparePackets(cmp1[i], cmp2[i])
                 if(order != "equal"):
                     break
                 
@@ -68,7 +70,7 @@ def comparePackets(input1, input2):
             break
             # return order
     
-    if order == "equal" and (len(input1) < len(input2) ):
+    if order == "equal" and (len(cmp1) < len(cmp2) ):
         order = "ok"
         # print("  - Left side ran out of items, so inputs are in the right order")
     
@@ -95,8 +97,10 @@ if __name__ == '__main__':
         packets = []
         
         # Adding the divider packets to the input data
-        packets.append(literal_eval("[[2]]"))
-        packets.append(literal_eval("[[6]]"))
+        divider1 = literal_eval("[[2]]")
+        divider2 = literal_eval("[[6]]")
+        packets.append(divider1)
+        packets.append(divider2)
         
         print("Initializing...", flush=True)
 
@@ -126,7 +130,24 @@ if __name__ == '__main__':
                 if (comparePackets(packets[j], packets[j+1]) == "nok"):
                     packets[j], packets[j+1] = packets[j+1], packets[j]        
         
-        print(packets)
+        print('\n'.join(map(str,packets)))
+        
+        cnt = 0
+        div1_pos = 0
+        div2_pos = 0
+        for x in packets:
+            cnt = cnt + 1
+            if (x == divider1):
+                print ("div1 found at ", cnt)
+                div1_pos = cnt
+            if (x == divider2):
+                print ("div2 found at ", cnt)
+                div2_pos = cnt
+        
+        print("Distress signal is ", div1_pos * div2_pos )
+    
+            
+        
         
     except RuntimeError:
         print("Finishing...", flush=True)
