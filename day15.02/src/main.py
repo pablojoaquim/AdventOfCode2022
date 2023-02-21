9b02f489160eaf8f8b120923a0f32daff9362161
Pablo Joaquim <pablojoaquim@gmail.com>
2023-02-21 11:56:07 -0300

    day15.02 - The previous method was too slow to look for in all the rows in a matrix of 4Mx4M points, so, I'm trying to find the only point which is outside of the all the circles. To do so I'm adding a function to determine the points at a certain distance (this function was created by chatGPS)

--- day15.02/src/main.py
+++ day15.02/src/main.py
@@ -41,34 +41,47 @@ def printMatrix (title, matrix):
 # * axes. In simple terms, it is the sum of absolute difference between the 
 # * measures in all dimensions of two points.
 # ******************************************************************************
-def calcManhattanDistance(p, q):
-    return sum(abs(val1-val2) for val1, val2 in zip(p,q))
+def manhattan_distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
 
 # ******************************************************************************
-# * @brief Read all the sensors and obtain the positions in a row without beacons
+# * @brief Return a list of the points with a Manhattan distance from a center point
 # ******************************************************************************
-def getPositionsWithoutBeacons(dataset, x_min, x_max, y):
-    positions_without_beacons = set()
-    graph = ['.']*(x_max-x_min)
+def find_points_with_distance(x, y, distance):
+    points = set()
+    for i in range(-distance, distance+1):
+        j = distance - abs(i)
+        points.add((x+i, y+j))
+        points.add((x+i, y-j))
+    return list(points)
+
+# # ******************************************************************************
+# # * @brief Read all the sensors and obtain the positions in a row without beacons
+# # ******************************************************************************
+# def getPositionsWithoutBeacons(dataset, x_min, x_max, y):
+#     positions_without_beacons = set()
+#     graph = ['.']*(x_max-x_min)
         
-    for data in dataset:
-        sensor = data[0]
-        beacon = data[1]
-        distance = data[2]
+#     for data in dataset:
+#         sensor = data[0]
+#         beacon = data[1]
+#         distance = data[2]
         
-        for x in range(x_min,x_max):
-            point = (x,y)
-            if (point == sensor):
-                graph[x] = 'S'
-            elif (point == beacon):
-                graph[x] = 'B'
-            elif (graph[x] == '.'):
-                distance_to_point = calcManhattanDistance(sensor, point)
-                if (distance_to_point <= distance):
-                    positions_without_beacons.add(point)
-                    graph[x] = '#'
+#         for x in range(x_min,x_max):
+#             point = (x,y)
+#             if (point == sensor):
+#                 graph[x] = 'S'
+#             elif (point == beacon):
+#                 graph[x] = 'B'
+#             elif (graph[x] == '.'):
+#                 distance_to_point = calcManhattanDistance(sensor, point)
+#                 if (distance_to_point <= distance):
+#                     positions_without_beacons.add(point)
+#                     graph[x] = '#'
+#                 elif(distance_to_point == distance+1):
+#                     graph[x] = '+'
                 
-    return (positions_without_beacons, graph)
+#     return (positions_without_beacons, graph)
                             
 # ******************************************************************************
 # * @brief The handler for the termination signal handler
@@ -119,19 +132,24 @@ if __name__ == '__main__':
                     reports.append([sensor, beacon, distance])
                     # reports[sensor] = (beacon, calcManhattanDistance(sensor, beacon))
                     
-        # print(reports)
+        print(reports)
         # print(len(reports))
         
-        x_min = 0
-        x_max = 0
-        max_distance = 0
-        for report in reports:
-            if (report[0][0]<x_min):
-                x_min = report[0][0]
-            if (report[0][0]>x_max):
-                x_max = report[0][0]
-            if (report[2]>max_distance):
-                max_distance = report[2]    
+        # x_min = 0
+        # x_max = 0
+        # max_distance = 0
+        # for report in reports:
+        #     if (report[0][0]<x_min):
+        #         x_min = report[0][0]
+        #     if (report[0][0]>x_max):
+        #         x_max = report[0][0]
+        #     if (report[2]>max_distance):
+        #         max_distance = report[2]
+
+        # print(x_min)
+        # print(x_max)
+        # print(max_distance)
+        
         
         # x_min = x_min - max_distance
         # x_max = x_max + max_distance
@@ -139,34 +157,52 @@ if __name__ == '__main__':
         # print(x_min)
         # print(x_max)
         # y=10
-        x_min=0
-        x_max=4000000
-        y_min=0
-        y_max=4000000
+        
+        # x_min=0
+        # x_max=4000000
+        # y_min=0
+        # y_max=4000000
         
         # x_min=0
         # x_max=20
         # y_min=0
         # y_max=20
          
-        graph = []        
-        for y in range(y_min,y_max):
-            positionsWithoutBeacon, rowGraph = getPositionsWithoutBeacons(reports, x_min, x_max, y)
-            graph.append(rowGraph)
-            if ('.' in rowGraph):
-                break
-            # print(graph)
-            # print (len(getPositionsWithoutBeacons(reports, x_min, x_max, y)))
+        # graph = []        
+        # for y in range(y_min,y_max):
+        #     positionsWithoutBeacon, rowGraph = getPositionsWithoutBeacons(reports, x_min, x_max, y)
+        #     graph.append(rowGraph)
+        # #     # if ('.' in rowGraph):
+        # #     #     break
+        # #     # print(graph)
+        # #     # print (len(getPositionsWithoutBeacons(reports, x_min, x_max, y)))
+        # #     # print (len(positionsWithoutBeacon))
         
         # printMatrix(None, graph)
         
-        # Look for the distress signal
-        for y in range(len(graph)):
-            for x in range(len(graph[y])):
-                if (graph[y][x] == '.'):
-                    print("x =", x," y =", y)
-                    distress_signal = 4000000 * x + y
-                    print("distress signal =", distress_signal)
+        # # Look for the distress signal
+        # for y in range(len(graph)):
+        #     for x in range(len(graph[y])):
+        #         if (graph[y][x] == '.'):
+        #             print("x =", x," y =", y)
+        #             distress_signal = 4000000 * x + y
+        #             print("distress signal =", distress_signal)
+        
+        for report in reports:
+            sensor = report[0]
+            distance = report[2]
+            points = find_points_with_distance(sensor[0], sensor[1], distance)
+            print(f"Puntos: {points}")
+            
+            # dataset = []
+            # dataset.append(report)
+            # graph = []
+            # for y in range(y_min,y_max):
+            #     positionsWithoutBeacon, rowGraph = getPositionsWithoutBeacons(dataset, x_min, x_max, y)
+            #     graph.append(rowGraph)        
+            # printMatrix(None, graph)
+            # print('\n')
+            # print('\n')
         
         
     except RuntimeError:
